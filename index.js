aa5dc38c1ac9695fb001bccef6c78384378219c0
Roberto <robertoleao1212@gmail.com>
2023-09-13 17:13:27 -0300

    fazendo botao que insere bnome

---
+++ index.js
@@ -0,0 +1,8 @@
+var ul = document.getElementById('menu')
+var inputComNome = document.getElementById('input')
+
+const btn = () => {
+    let nome = document.createElement('li');
+    ul.innerHTML = `Seja bem vindo ${inputComNome}`;
+    ul.appendChild(nome);
+}
